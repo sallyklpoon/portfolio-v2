58f5a33f46becfaac9d350317820399285dd580c
Sally Poon <sallypoon@hotmail.com>
2023-10-22 22:20:22 -0700

    upgrade babel

---
+++ src/theme/theme.ts
@@ -0,0 +1,32 @@
+export const theme = {
+    colors: {
+        primary: {
+            50: '#f2fdfc',
+            100: '#c4f8ef',
+            200: '#adf6e9',
+            300: '#97f3e3',
+            400: '#80f0dd',
+            500: '#52ebd1',
+            600: '#2de6c5',
+            700: '#19d8b7',
+            800: '#16c1a4',
+            900: '#11937d'
+        },
+        secondary: {
+            50: '#f3faeb',
+            100: '#d9f1c2',
+            200: '#ccecad',
+            300: '#bfe798',
+            400: '#b3e383',
+            500: '#99d95a',
+            600: '#7fcf31',
+            700: '#73bb2b',
+            800: '#66a726',
+            900: '#4d7d1d'
+        }
+    },
+    fonts: {
+        heading: "Palanquin Dark",
+        body: "'Open Sans', sans-serif'"
+    }
+};
